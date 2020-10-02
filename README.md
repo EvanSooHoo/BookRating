8635053d6b31671546012550bf2b6efd6562e777
evansoohoo <evan.soohoo@gmail.com>
2020-10-02 14:54:42 -0700

    first (and possibly only) commit

---
+++ README.md
@@ -0,0 +1,26 @@
+THE SIMPLEST AND MOST SPECIFIC WEBSITE I HAVE EVER MADE
+
+# bookrating
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-app>
+    <v-card class="elevation-16 mx-auto" width="300">
+      <v-card-title class="headline justify-center">
+        <center>Book Rating</center>
+      </v-card-title>
+      <v-card-text>
+        <div class="text-center mt-12">
+          <center><h2>Plot</h2></center>
+          <v-rating
+            v-model="rating"
+            color="yellow darken-3"
+            background-color="grey darken-1"
+            empty-icon="$ratingFull"
+            half-increments
+            hover
+          ></v-rating>
+        </div>
+      </v-card-text>
+      <v-card-text>
+        <div class="text-center mt-12">
+          <center><h2>Dialogue</h2></center>
+          <v-rating
+            v-model="rating"
+            color="yellow darken-3"
+            background-color="grey darken-1"
+            empty-icon="$ratingFull"
+            half-increments
+            hover
+          ></v-rating>
+        </div>
+      </v-card-text>
+      <v-card-text>
+        <div class="text-center mt-12">
+          <center><h2>Writing Style</h2></center>
+          <v-rating
+            v-model="rating"
+            color="yellow darken-3"
+            background-color="grey darken-1"
+            empty-icon="$ratingFull"
+            half-increments
+            hover
+          ></v-rating>
+        </div>
+      </v-card-text>
+      <v-card-text>
+        <div class="text-center mt-12">
+          <center><h2>Characters</h2></center>
+          <v-rating
+            v-model="rating"
+            color="yellow darken-3"
+            background-color="grey darken-1"
+            empty-icon="$ratingFull"
+            half-increments
+            hover
+          ></v-rating>
+        </div>
+      </v-card-text>
+      <v-card-text>
+        <div class="text-center mt-12">
+          <center><h2>Overall</h2></center>
+          <v-rating
+            v-model="rating"
+            color="yellow darken-3"
+            background-color="grey darken-1"
+            empty-icon="$ratingFull"
+            half-increments
+            hover
+          ></v-rating>
+        </div>
+      </v-card-text>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    //
+  })
+};
+</script>
